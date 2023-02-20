579b80ccc376bf49f8ae4a6da6dfc9bb0b0acb04
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-02-20 13:05:18 +0300

    refactor [functions.js]: resolving after code-review

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,28 @@
-function isStrLengthValid (str, limit) {
-  return str.length <= limit;
-}
+const isStrLengthValid = (str, limit) => str.length <= limit;
 
-function isPaliondrome (str) {
+const reverseStr = (str) => str.split('').reverse().join('');
+
+const isPaliondrome = (str) => {
   const processedStr = str.replace(/ /g, '').toLowerCase();
-  let reversedStr = '';
-  for (let i = 1; i <= processedStr.length; i++) {
-    reversedStr += processedStr[processedStr.length - i];
-  }
+  const reversedStr = reverseStr(processedStr);
   return processedStr === reversedStr;
-}
+};
 
-function extractNumber (str) {
+const extractNumber = (str) => {
   if (typeof(str) === 'number') {
     str += '';
   }
   return parseInt(str.replace(/\D/g, ''), 10);
-}
+};
 
-function addFillToStr (str, minLength, filler) {
+const addFillToStr = (str, minLength, filler) => {
   let resultStr = str;
   while (resultStr.length < minLength) {
     const charsToAdd = minLength - resultStr.length;
     resultStr = filler.slice(0, charsToAdd) + resultStr;
   }
   return resultStr;
-}
+};
 
 isStrLengthValid('abc', 3);
 isPaliondrome('abc');
