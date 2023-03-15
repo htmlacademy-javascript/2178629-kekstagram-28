c770afa76b21f448c0147f0cc4a3c035c528324f
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-03-15 20:55:28 +0300

    refactor [card-modal.js] add functions

--- js/card-modal.js
+++ js/card-modal.js
@@ -8,24 +8,32 @@ const closeBtn = bigCard.querySelector('.big-picture__cancel');
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    bigCard.classList.add('hidden');
-    document.removeEventListener('keydown', onDocumentKeydown);
+    hideBigCard();
   }
 };
 
 const onCloseBtnClick = () => {
+  hideBigCard();
+};
+
+function hideBigCard() {
   bigCard.classList.add('hidden');
+  closeBtn.removeEventListener('click', onCloseBtnClick);
   document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+const unhideBigCard = () => {
+  bigCard.classList.remove('hidden');
+  closeBtn.addEventListener('click', onCloseBtnClick);
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
 const onCardsClick = (evt) => {
   if (evt.target.closest('.picture')) {
-    bigCard.classList.remove('hidden');
+    unhideBigCard();
     bigCard.querySelector('.social__comment-count').classList.add('hidden'); // временная заглушка
     bigCard.querySelector('.comments-loader').classList.add('hidden'); // временная заглушка
-    closeBtn.addEventListener('click', onCloseBtnClick);
     document.querySelector('body').classList.add('.modal-open');
-    document.addEventListener('keydown', onDocumentKeydown);
 
     const publicationImgUrl = evt.target.closest('.picture').querySelector('.picture__img').getAttribute('src');
 
