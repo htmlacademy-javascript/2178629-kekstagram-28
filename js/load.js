e24eaac7e6b26aa368e4a7f570b43ab8a919be08
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-03-30 21:29:14 +0300

    feat [load.js]: add load molul functionality

--- js/load.js
+++ js/load.js
@@ -1,6 +1,17 @@
-fetch('https://28.javascript.pages.academy/kekstagram/data')
-  .then((response) => {
-    if (response.ok) {
-      console.log('good');
-    }
-  });
+const loader = (onSucsess, onError) => () => {
+  fetch('https://28.javascript.pages.academy/kekstagram/data',
+    {
+      method: 'GET'
+    })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error(`${response.status} ${response.statusText} (статусный текст ошибки)`);
+    })
+    .then((data) => onSucsess(data))
+    .catch((err) => onError(err));
+};
+
+export { loader };
--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ import { publications } from './mock-publications.js';
 import { renderCards } from './cards.js';
 import { activateBigPicture } from './card-modal.js';
 import { activateUploadModal } from './upload-modal.js';
-import './load.js';
+import { loader } from './load.js';
+
+const loadPublications = loader(console.log, console.error);
 
 renderCards(publications);
 activateBigPicture();
 activateUploadModal();
+
+loadPublications();
