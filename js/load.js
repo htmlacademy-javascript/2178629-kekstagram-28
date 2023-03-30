daf717d3d06abae9015f177821673ad691196593
BlindDenn <blinddenn@gmail.com>
2023-03-30 19:51:49 +0300

    feat [load.js]: add load.js module

---
+++ js/load.js
@@ -0,0 +1,6 @@
+fetch('https://28.javascript.pages.academy/kekstagram/data')
+  .then((response) => {
+    if (response.ok) {
+      console.log('good');
+    }
+  });
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { publications } from './mock-publications.js';
 import { renderCards } from './cards.js';
 import { activateBigPicture } from './card-modal.js';
 import { activateUploadModal } from './upload-modal.js';
-import './upload-effects.js';
+import './load.js';
 
 renderCards(publications);
 activateBigPicture();
