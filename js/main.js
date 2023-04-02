0f973114dcd1f0a8bb9d827bc416f99fca29da0e
BlindDenn <104034214+BlindDenn@users.noreply.github.com>
2023-04-02 12:14:50 +0300

    refactor [main.js, publications.js]: clean up main.js code, add publications module

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-import { renderCards } from './render-cards.js';
-import { activateBigPicture } from './card-modal.js';
 import { activateUploadModal } from './upload-modal.js';
-import {
-  getPublications,
-  onGetDataError
-} from './api.js';
+import { renderPublications } from './publications.js';
 
-// let publications = [];
-
-(async () => {
-  try {
-    const publications = await getPublications ();
-    renderCards(publications);
-    activateBigPicture(publications);
-  } catch(err) {
-    onGetDataError();
-  }
-})();
-
+renderPublications();
 activateUploadModal();
